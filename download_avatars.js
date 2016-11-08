0690521dedc02c68ffdbcc4f27850aaaa5287e4b
Jungmin Cha <ckwjdals90@gmail.com>
2016-11-08 02:13:51 +0000

    add requestedULR to download.avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -2,12 +2,27 @@
 const request = require('request');
 const fs = require('fs');
 
-var requestOptions = request('https://api.github.com/repos/jquery/jquery/contributors').on('response', (response) => {
+console.log('Welcome to the GitHub Avatar Downloader!');
 
-  console.log('Response Status Code: ', response.statusCode);
-  console.log('Response Message: ', response.statusMessage);
-  console.log('Response Content Type: ', response.headers['content-type']);
+function getRepoContributors(repoOwer, repoName, cb) {
+  var GITHUB_USER = "ckwjdals90"
+  var GITHUB_TOKEN = "6294b5e65259a9d5a7be5c07c64e9821977418fe"
 
+  var requestULR = `https://{$GITHUB_USER}:{$GITHUB_TOKEN}@api.github.com/repos/{$repoOwner}/{$repoName}/contributors`;
+    console.log(requestULR);
+
+}
+getRepoContributors("jquery", "jquery", (err, result) => {
+  console.log("Errors:", err);
+  console.log("Result:", result);
 })
 
-console.log(requestOptions);
+// var requestOptions = request('https://api.github.com/repos/jquery/jquery/contributors').on('response', (response) => {
+
+//   console.log('Response Status Code: ', response.statusCode);
+//   console.log('Response Message: ', response.statusMessage);
+//   console.log('Response Content Type: ', response.headers['content-type']);
+
+// })
+
+// console.log(requestOptions);
