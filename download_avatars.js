f79d30659283ecce8e1390c6d3791902c51ad51f
Jungmin Cha <ckwjdals90@gmail.com>
2016-11-08 04:51:24 +0000

    added notes for my understanding

--- download_avatars.js
+++ download_avatars.js
@@ -6,15 +6,17 @@ const repo = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-//creating a function that will fetch the array of contributors
+//configuring a function that will fetch the contributors data
 function getRepoContributors(repoOwner, repoName, cb) {
   var GITHUB_USER = "ckwjdals90"
   var GITHUB_TOKEN = "6294b5e65259a9d5a7be5c07c64e9821977418fe"
 
-  // the
+  // the URL is created for request
   var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  // var requestURL = `https://{$GITHUB_USER}:{$GITHUB_TOKEN}@api.github.com/repos/{$repoOwner}/{$repoName}/contributors`;      //not working..
 
-  var options = {
+  // creating a key for authorization
+  var authorizedURL = {
       url: requestURL,
       headers: {
         'User-Agent': 'why do you even care',
@@ -22,36 +24,35 @@ function getRepoContributors(repoOwner, repoName, cb) {
       }
     }
 
-  request(options, (error, response, body) => {
-    var contributors = JSON.parse(body);
+  // getRepoContributors function initializes the request
+  request(authorizedURL, (error, response, body) => {
+    // console.log('Response Status Code: ', response.statusCode);                  // checking
+    // console.log('Response Message: ', response.statusMessage);                   // response
+    // console.log('Response Content Type: ', response.headers['content-type']);)   // status
+    var contributors = JSON.parse(body);      // parsing the data received into usable format
 
-    cb(error, contributors);
+    cb(error, contributors);    // initializing the callback of getRepoContributors function.
   });
 
 }
 
-
-getRepoContributors(owner, repo, (err, contributors) => {
-  // console.log("Errors:", err);
-  // console.log("Result:", contributors);
-
+// executing getRepoContributors function
+getRepoContributors(owner, repo, (err, contributors) => {     // configuring the callback function
   //loop through the contributors array
+  return contributors.forEach(function(contributor) {
     //url as contributors.avatar_url
     //filePath as contributors.login
-
-  return contributors.forEach(function(contributor) {
     downloadImageByURL(contributor.avatar_url, contributor.login);
 })
 });
 
 
 function downloadImageByURL(url, filePath) {
-console.log(url);
-console.log(filePath);
+// console.log(url);         // checking avatar_url of the input
+// console.log(filePath);    // checking id of the input
   request(url).pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'));
 
 }
 
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
 
 
